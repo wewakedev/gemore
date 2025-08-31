f9316d21534de428c7ca860d6e5c41917551f192
ArreX <25535377+Kamaldixit@users.noreply.github.com>
2025-09-01 02:49:25 +0530

    Phonepe payment bug fixe

--- database/migrations/2025_08_31_07230432_create_orders_table.php
+++ database/migrations/2025_08_31_07230432_create_orders_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->string('order_number')->unique();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->nullable()->constrained()->onDelete('cascade');
             
             // Shipping Address
             $table->json('shipping_address');
